912c4462cc75c02431d35867de5da1a521b4d122
Mindy Johnson <mindy.l.brock@gmail.com>
2021-06-24 21:24:40 -0500

    Added function to play a round.

--- script.js
+++ script.js
@@ -12,10 +12,38 @@ function computerPlay() {
             randomSelection = "Scissors";
             break;
         default:
-            randomSelection = "error";
+            randomSelection = "Rock";
         }
 
     return randomSelection;
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    let roundResult = "Dud! Noone wins!";
+    playerSelection = playerSelection.charAt(0).toUpperCase() 
+        + playerSelection.substr(1, playerSelection.length).toLowerCase();
+
+    switch(true) {
+        case (playerSelection === computerSelection):
+            roundResult = `It's a tie! ${playerSelection} vs. ${computerSelection}`;
+            break;
+        case (playerSelection === "Rock" && computerSelection === "Scissors"):
+        case (playerSelection === "Paper" && computerSelection === "Rock"):
+        case (playerSelection === "Scissors" && computerSelection === "Paper"):
+            roundResult = `You Win! ${playerSelection} beats ${computerSelection}`;
+            break;
+        case (computerSelection === "Rock" && playerSelection === "Scissors"):
+        case (computerSelection === "Paper" && playerSelection === "Rock"):
+        case (computerSelection === "Scissors" && playerSelection === "Paper"):
+            roundResult = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            break;
+        default:
+            roundResult = "Uhoh! Something went wrong."
+    }
+
+    return roundResult;
+}
+
+const computerSelection = computerPlay();
+
+console.log(playRound("roCK", computerSelection));
